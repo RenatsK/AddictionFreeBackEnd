3af648b748024c539e3ac58bf4f691af2368c8ba
Renat <renat.kuzmin.g@gmail.com>
2023-12-27 21:59:02 +0100

    Small changes added

--- routes/login.js
+++ routes/login.js
@@ -6,13 +6,19 @@ login.post('/', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        db.query('SELECT * FROM User WHERE email = ? AND password = ?', [email, password], function(error, results) {
+        db.query('SELECT * FROM User WHERE email = ? AND password = ?', [email, password], function (error, results) {
+            if (error) {
+                console.error('Error during database query:', error);
+                res.status(500).json({ success: false, message: 'Error during login' });
+                return;
+            }
             if (results.length > 0) {
                 res.json({ success: true });
+                
             } else {
                 res.status(401).json({ success: false, message: 'Invalid email or password' });
             }
-        });       
+        });
     } catch (error) {
         console.error('Error during login:', error);
         res.status(500).json({ success: false, message: 'Error during login' });
